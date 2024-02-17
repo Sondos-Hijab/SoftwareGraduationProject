0140461e087f89a43c1317c7310a0a115a2ed0a9
Sondos Hijab <sondos.hijab@gmail.com>
2024-02-17 23:21:39 +0200

    adding customized colors

--- fontend/software_grad_project/lib/core/constants/colors.dart
+++ fontend/software_grad_project/lib/core/constants/colors.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryBlue = Color(0xFF13B6F5);
+  static const Color primaryGreen = Color(0xFF0db783);
+  static const Color primaryDarkBlue = Color(0xFF2f47c6);
+  static const Color primaryYellow = Color(0xFFfac100);
 
+  static const Color black = Color(0xFF242424);
+  static const Color lightGrey = Color(0xFFa4a4a4);
   static const Color grey = Color(0xFF696969);
+
+  static const Color appWhite = Color(0xFFfafafa);
 }
