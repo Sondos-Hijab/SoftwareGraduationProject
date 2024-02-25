7e9e217aada1a44354710c424d80496990166f88
Sondos Hijab <sondos.hijab@gmail.com>
2024-02-25 20:39:01 +0200

    working on the home page, adding the notification and header, slider

--- fontend/software_grad_project/lib/view/screens/app-main-pages/homepage.dart
+++ fontend/software_grad_project/lib/view/screens/app-main-pages/homepage.dart
@@ -1,20 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:software_grad_project/core/constants/colors.dart';
+import 'package:software_grad_project/core/constants/imagesassets.dart';
+import 'package:software_grad_project/core/services/service.dart';
+import 'package:software_grad_project/data/datasource/static/static.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final myServices = Get.find<MyServices>();
+    String? username = myServices.sharedPreferences.getString("username");
+
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppColors.appWhite,
-        elevation: 0.0,
-        title: Text(
-          'Home Page',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
+      body: SafeArea(
+        child: ListView(children: [
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome Back",
+                        style:
+                            Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                  fontSize: 20,
+                                ),
+                      ),
+                      Text(
+                        "$username",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .copyWith(
+                                fontSize: 25,
+                                color: AppColors.primaryBlue,
+                                fontWeight: FontWeight.w700),
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications_active_outlined,
+                      size: 35,
+                      color: Colors.grey.shade700,
+                    ))
+              ],
+            ),
+          ),
+          Container(
+            height: 200,
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Stack(
+              children: [
+                PageView.builder(
+                  itemCount: sliderList.length,
+                  itemBuilder: ((context, index) => ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Stack(
+                          children: [
+                            Image.asset(
+                              sliderList[index].image!,
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                              height: double.infinity,
+                            ),
+                            Positioned.fill(
+                              child: Container(
+                                color: Colors.black.withOpacity(
+                                    0.3), // Adjust opacity as needed
+                              ),
+                            ),
+                            Positioned.fill(
+                              child: Center(
+                                child: Text(
+                                  sliderList[index].title!,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 30,
+                                      fontFamily: "Anton"),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
