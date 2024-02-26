a4f19ad6bcad8dd3b73b858a1c90691dce8ecce2
Sondos Hijab <sondos.hijab@gmail.com>
2024-02-26 23:40:28 +0200

    preparing the model for the feedback that will be recieved

--- fontend/software_grad_project/lib/data/datasource/static/static.dart
+++ fontend/software_grad_project/lib/data/datasource/static/static.dart
@@ -1,4 +1,5 @@
 import 'package:software_grad_project/core/constants/imagesassets.dart';
+import 'package:software_grad_project/data/model/feedbackmodel.dart';
 import 'package:software_grad_project/data/model/onboardingmodel.dart';
 import 'package:software_grad_project/data/model/slidermodel.dart';
 
@@ -25,4 +26,37 @@ List categoriesPictures = [
   AppImageAssets.clothesCategory,
   AppImageAssets.devicesCategory,
   AppImageAssets.restaurantCategory
+];
+
+List<FeedbackModel> feedbackData = [
+  FeedbackModel(
+    "John Doe",
+    AppImageAssets.profileImage,
+    "Doe's Cafe",
+    "I had a wonderful experience at Doe's Cafe! The ambiance was cozy, the food was delicious, and the staff was very attentive. I especially loved their signature dish, it was simply divine. I highly recommend this place to anyone looking for a great dining experience.",
+    "assets/images/feedbackImages/cafe.jpg",
+    4,
+    3,
+    2,
+  ),
+  FeedbackModel(
+    "Alice Smith",
+    AppImageAssets.profileImage,
+    "Smith's Electronics",
+    "I recently purchased a new laptop from Smith's Electronics and I couldn't be happier with my purchase! The ordering process was smooth, the delivery was prompt, and the product itself exceeded my expectations. It's rare to find such excellent customer service these days. I will definitely be a returning customer!",
+    "assets/images/feedbackImages/devices.jpg",
+    5,
+    2,
+    4,
+  ),
+  FeedbackModel(
+    "Bob Johnson",
+    AppImageAssets.profileImage,
+    "Johnson's Bakery",
+    "Johnson's Bakery never disappoints! Every time I visit, I'm greeted with the heavenly aroma of freshly baked bread and pastries. The quality of their products is outstanding and the staff is always friendly and helpful. It's my go-to place for baked goods, and I highly recommend it to anyone with a sweet tooth!",
+    "assets/images/feedbackImages/bakery.jpg",
+    4,
+    4,
+    5,
+  ),
 ];
