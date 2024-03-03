ec474830dc3db70cc29cb48997bd035c187d9b16
Sondos Hijab <sondos.hijab@gmail.com>
2024-03-03 08:03:03 +0200

    adding the basics page for the searching functionality

---
+++ frontend/software_grad_project/lib/view/widgets/search-page/custome_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final String titleappbar;
+  final void Function()? onPressedSearch;
+  final void Function(String)? onChanged;
+  final TextEditingController? searchController;
+  const CustomAppBar(
+      {Key? key,
+      required this.titleappbar,
+      this.onPressedSearch,
+      this.onChanged,
+      this.searchController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Expanded(
+          child: TextFormField(
+        controller: searchController,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+            prefixIcon: IconButton(
+                icon: Icon(Icons.search), onPressed: onPressedSearch),
+            hintText: titleappbar,
+            hintStyle: TextStyle(fontSize: 18),
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(10)),
+            filled: true,
+            fillColor: Colors.grey[200]),
+      )),
+    ]);
+  }
+}
