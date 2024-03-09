fe6e1e4cacc2c2cb3b2fbaa4304f7451ad793e99
raghadSabri6 <raghadsabri1111@gmail.com>
2024-03-09 12:14:36 +0200

    Update resetPasswordEmail.js, generateTempToken.js, and softwaredb.sql

--- backend/software_grad_project/HelperConstants/resetPasswordEmail.js
+++ backend/software_grad_project/HelperConstants/resetPasswordEmail.js
@@ -2,7 +2,7 @@ const emailContent = {
   resetPassword: {
     name: "Reset Password",
     intro: "Your password reset request has been received successfully.",
-    outro: "Please ensure that this OTP is only for verification by 3 min.",
+    outro: "Please ensure that this OTP is only for verification by 5 min.",
   },
 };
 
