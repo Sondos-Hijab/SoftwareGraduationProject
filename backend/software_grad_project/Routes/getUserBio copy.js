752d5db413db8ae92def5b97c2fc7f3679057a7d
raghadSabri6 <raghadsabri1111@gmail.com>
2024-03-22 19:50:35 +0200

    Removed duplicate file getUserBio copy.js

--- backend/software_grad_project/Routes/getUserBio copy.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticateToken = require('../HelperObjects/checkAuth');
-const { getUserBio }  = require('../RouteImplementations/getUserBio');
-
-router.get('/user/getUserBio', authenticateToken, getUserBio);
-
-module.exports = router;
-
