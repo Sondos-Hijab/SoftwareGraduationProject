8b13e698ec86f6ecb24c57219ad013748cbb51ef
Sondos Hijab <sondos.hijab@gmail.com>
2024-04-10 19:22:34 +0300

    adding the front-back connection of gender and age ratios for dashboard

--- frontend-web/src/apis/dashboardRequests.js
+++ frontend-web/src/apis/dashboardRequests.js
@@ -17,3 +17,75 @@ export async function getAvgRate(rateType, startDate, endDate) {
     if (error.response) return error.response.data;
   }
 }
+
+export async function getFeedbackGenderRatio() {
+  const accessToken = localStorage.getItem("accessToken");
+  const businessName = localStorage.getItem("businessName");
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/RateRelay/user/feedbackGenderRatio?businessName=${businessName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response) return error.response.data;
+  }
+}
+
+export async function getFollowGenderRatio() {
+  const accessToken = localStorage.getItem("accessToken");
+  const businessName = localStorage.getItem("businessName");
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/RateRelay/user/followGenderRatio?businessName=${businessName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response) return error.response.data;
+  }
+}
+
+export async function getFeedbackAgeRatio() {
+  const accessToken = localStorage.getItem("accessToken");
+  const businessName = localStorage.getItem("businessName");
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/RateRelay/user/feedbackAgeRatio?businessName=${businessName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response) return error.response.data;
+  }
+}
+
+export async function getFollowAgeRatio() {
+  const accessToken = localStorage.getItem("accessToken");
+  const businessName = localStorage.getItem("businessName");
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/RateRelay/user/followAgeRatio?businessName=${businessName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response) return error.response.data;
+  }
+}
