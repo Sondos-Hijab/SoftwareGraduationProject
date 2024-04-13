3b6320aeb9e6ee049aa5edd53c91d814686ea679
Sondos Hijab <sondos.hijab@gmail.com>
2024-04-13 23:08:03 +0200

    added feedback analysis connection

--- frontend-web/src/helper-components/Dashboard/FeedbackAnalysis.jsx
+++ frontend-web/src/helper-components/Dashboard/FeedbackAnalysis.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import MaleFemalePieChart from "./MaleFemalePieChart";
 import FeedbackAgeRatioChart from "./FeedbackAgeRatioChart";
+import {
+  getFeedbackAgeRatio,
+  getFeedbackGenderRatio,
+} from "@/apis/dashboardRequests";
 
 const FeedbackAnalysis = () => {
   const [male, setMale] = useState();
@@ -10,20 +14,38 @@ const FeedbackAnalysis = () => {
   const [moreThan30, setMoreThan30] = useState(0);
 
   useEffect(() => {
-    //get values
+    getFeedbackAgeRatio().then((value) => {
+      if (value?.error) console.log(value.error);
+      else {
+        setLessThan18(value["below18Percentage"]);
+        setBetween18and30(value["age18to30Percentage"]);
+        setMoreThan30(value["above30Percentage"]);
+      }
+    });
+    getFeedbackGenderRatio().then((value) => {
+      if (value?.error) console.log(value.error);
+      else {
+        setFemale(value.femalePercentage);
+        setMale(value.malePercentage);
+      }
+    });
   }, []);
 
   return (
     <div className="mb-2 shadow-lg rounded-xl p-4 flex flex-col lg:flex-row justify-center items-center flex-wrap">
       {/* chart */}
       <FeedbackAgeRatioChart
-        rates={{ lessThan18: 15, between18and30: 50, moreThan30: 20 }}
+        rates={{
+          lessThan18: lessThan18,
+          between18and30: between18and30,
+          moreThan30: moreThan30,
+        }}
         title="Feedback writing age ratio"
       />
 
       <MaleFemalePieChart
-        male={14}
-        female={25}
+        male={male}
+        female={female}
         title="Feedback writing gender ratio"
       />
     </div>
