ca07c88555c08c0bdf88225865fe9a3bcacef6db
raghadSabri6 <146375237+raghadSabri6@users.noreply.github.com>
2024-05-13 21:55:43 +0300

    Delete backend/software_grad_project/Routes/getChatMessages.js

--- backend/software_grad_project/Routes/getChatMessages.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticateToken = require('../HelperObjects/checkAuth');
-const getChatMessages = require('../RouteImplementations/getChatMessages');
-
-router.get('/user/getChatMessages', authenticateToken, getChatMessages);
-
-module.exports = router;
