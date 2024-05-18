55d8778ab103f16f2aef1fcc663755404c8e172e
Sondos Hijab <sondos.hijab@gmail.com>
2024-05-18 19:35:10 +0300

    Updating imports

--- frontend-web/src/_root/pages/index.js
+++ frontend-web/src/_root/pages/index.js
@@ -3,7 +3,6 @@ export { default as Profile } from "./Profile";
 export { default as CreatePost } from "./CreatePost";
 export { default as Posts } from "./Posts";
 export { default as FeedbackPage } from "./FeedbackPage";
-export { default as Chatting } from "./Chatting";
 export { default as Notifications } from "./Notifications";
 export { default as User } from "./User";
 export { default as BusinessPage } from "./BusinessPage";
