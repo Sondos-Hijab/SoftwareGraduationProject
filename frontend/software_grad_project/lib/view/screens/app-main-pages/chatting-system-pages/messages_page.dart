5fb063d68951caa8bbe4b2ffe8a66b413309fd1a
Sondos Hijab <sondos.hijab@gmail.com>
2024-05-19 13:29:53 +0300

    Working on messages page

--- frontend/software_grad_project/lib/view/screens/app-main-pages/chatting-system-pages/messages_page.dart
+++ frontend/software_grad_project/lib/view/screens/app-main-pages/chatting-system-pages/messages_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:software_grad_project/controller/app-main-pages-controllers/messages_page_controller.dart';
 import 'package:software_grad_project/core/constants/colors.dart';
+import 'package:software_grad_project/core/constants/routes_names.dart';
 import 'package:software_grad_project/view/widgets/chatting-system-widgets/message_card_view.dart';
 
 class MessagesPage extends StatelessWidget {
@@ -7,20 +10,44 @@ class MessagesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppColors.appWhite,
-        elevation: 0.0,
-        title: Text(
-          'Messages Page',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: 15,
-        itemBuilder: (index, context) => MessageCardView(),
-      ),
-    );
+    Get.put(MessagesPageControllerImp());
+    return GetBuilder<MessagesPageControllerImp>(builder: (controller) {
+      return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: AppColors.appWhite,
+            elevation: 0.0,
+            title: Text(
+              'Messages Page',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.refresh),
+                onPressed: () {
+                  controller.refreshChatPartnersList();
+                },
+              ),
+            ],
+          ),
+          body: Column(
+            children: List.generate(
+              controller.businessesList!.length,
+              (index) => MessageCardView(
+                businessName: controller.businessesList![index].businessName,
+                picture: controller.businessesList![index].picture,
+                onTap: () {
+                  Get.toNamed(
+                    AppRoutes.chatPage,
+                    arguments: {
+                      'businessName':
+                          controller.businessesList![index].businessName
+                    },
+                  );
+                },
+              ),
+            ),
+          ));
+    });
   }
 }
