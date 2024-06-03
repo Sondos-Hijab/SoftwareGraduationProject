8733b8ddd455e3d15566b56cbfd94ed300e7e987
Sondos Hijab <sondos.hijab@gmail.com>
2024-06-03 06:10:19 +0300

    Making chat page responsive and adding special functionality when the website is being used from mobile

--- frontend-web/src/_chat/ChatLayout.jsx
+++ frontend-web/src/_chat/ChatLayout.jsx
@@ -3,14 +3,31 @@ import UsersChats from "@/helper-components/Messages/UsersChats";
 import { Link, Outlet } from "react-router-dom";
 import { getBusinessChatPartners } from "@/apis/chatRequests";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faHouse,
+  faSyncAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ChatLayout = () => {
   const [chatPartners, setChatPartners] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
+  const [hideRecentChats, setHideRecentChats] = useState(false);
 
   useEffect(() => {
     refreshChatPartners();
-  }, []);
+    // Add event listener for screen size
+    window.addEventListener("resize", handleResize);
+    // Check initial screen size
+    handleResize();
+    console.log(isMobile);
+    // Cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isMobile]);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
 
   const refreshChatPartners = () => {
     getBusinessChatPartners().then((value) => {
@@ -18,50 +35,69 @@ const ChatLayout = () => {
     });
   };
 
+  const chatClicked = () => {
+    setHideRecentChats(true);
+  };
+
   return (
     <div className="grid grid-cols-6 gap-4 p-6 shadow-sm h-screen">
       <div className=" col-span-6 md:col-span-2 p-4 border rounded-md">
         <div className="flex justify-between items-center">
           {/* Icon for refresh */}
-          <Link
-            to="/"
-            onClick={refreshChatPartners}
-            className="cursor-pointer text-customYellow"
-          >
-            <FontAwesomeIcon icon={faHouse} size={24} />
-          </Link>
+          {!hideRecentChats ? (
+            <Link to="/" className="cursor-pointer text-customYellow">
+              <FontAwesomeIcon icon={faHouse} size="lg" />
+            </Link>
+          ) : (
+            <Link
+              to="/chatting"
+              className="cursor-pointer text-customYellow"
+              onClick={() => {
+                setHideRecentChats(false);
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowLeft} size="lg" />
+            </Link>
+          )}
 
-          <div className="flex items-center">
-            <h1 className="text-2xl font-semibold text-customPurple">
-              Recent Chats
-            </h1>
-          </div>
+          {!hideRecentChats && (
+            <div className="flex items-center">
+              <h1 className="text-2xl font-semibold text-customPurple">
+                Recent Chats
+              </h1>
+            </div>
+          )}
 
           {/* Icon for refresh */}
-          <div
-            onClick={refreshChatPartners}
-            className="cursor-pointer text-customYellow"
-          >
-            <FontAwesomeIcon icon={faSyncAlt} size={24} />
-          </div>
+          {!hideRecentChats && (
+            <div
+              onClick={refreshChatPartners}
+              className="cursor-pointer text-customYellow"
+            >
+              <FontAwesomeIcon icon={faSyncAlt} size="lg" />
+            </div>
+          )}
         </div>
 
         {/* search */}
         {/* <UserChatSearch /> */}
 
         {/* Users Chats */}
-        {chatPartners.length > 0 ? (
-          <UsersChats chatPartners={chatPartners} />
-        ) : (
-          <p className="text-center text-gray-500 h-full flex justify-center items-center">
-            It seems like you don't have any recent chats. Start a conversation
-            with someone to get chatting!
-          </p>
+        {!(hideRecentChats && isMobile) && chatPartners.length > 0 && (
+          <UsersChats
+            chatPartners={chatPartners}
+            isMobile={isMobile}
+            chatClicked={chatClicked}
+          />
         )}
+
+        {isMobile && <Outlet />}
       </div>
-      <div className=" col-span-4 hidden md:block px-4 pt-4 border rounded-md relative">
-        <Outlet />
-      </div>
+      {!isMobile && (
+        <div className=" col-span-4 hidden md:block px-4 pt-4 border rounded-md relative">
+          <Outlet />
+        </div>
+      )}
     </div>
   );
 };
--- frontend-web/src/helper-components/Messages/UsersChats.jsx
+++ frontend-web/src/helper-components/Messages/UsersChats.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import UserChatCard from "./UserChatCard";
 
-const UsersChats = ({ chatPartners }) => {
+const UsersChats = ({ chatPartners, isMobile, chatClicked }) => {
   return (
     <div className=" overflow-auto h-[80vh]">
       <div className="rounded-lg border-2 border-gray-100 bg-white mt-4">
         {chatPartners.map((partner) => (
-          <UserChatCard key={partner.userName} userData={partner} />
+          <UserChatCard
+            key={partner.userName}
+            userData={partner}
+            isMobile={isMobile}
+            chatClicked={chatClicked}
+          />
         ))}
       </div>
     </div>
