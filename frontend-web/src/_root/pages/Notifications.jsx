61e527a7c2ad6baf6af7b5b04f70287b1e77137d
Sondos Hijab <sondos.hijab@gmail.com>
2024-06-08 17:25:08 +0300

    Adding socket connection code for notifications web

--- frontend-web/src/_root/pages/Notifications.jsx
+++ frontend-web/src/_root/pages/Notifications.jsx
@@ -2,6 +2,7 @@ import { fetchFeedback } from "@/apis/feedbackRequests";
 import NotificationCard from "@/helper-components/Cards/NotificationCard";
 import { sortByDate } from "@/utils/utils";
 import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 const Notifications = () => {
   const [feedback, setFeedback] = useState([]);
@@ -12,8 +13,37 @@ const Notifications = () => {
         console.log("error fetching notifications");
       } else {
         setFeedback(value.feedback);
+        console.log(value.feedback);
       }
     });
+  }, []);
+
+  const SOCKET_URL = "http://localhost:3000";
+
+  const socket = io(SOCKET_URL, {
+    transports: ["websocket"],
+  });
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(`Connected with socket ID: ${socket.id}`);
+      socket.emit("register", {
+        businessName: localStorage.getItem("businessName"),
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from socket server");
+    });
+
+    socket.on("newFeedback", (newFeedback) => {
+      console.log("new feedback", newFeedback);
+      // setFeedback([...feedback, newFeedback]);
+    });
+
+    return () => {
+      socket.off("newFeedback");
+    };
   }, []);
 
   return (
