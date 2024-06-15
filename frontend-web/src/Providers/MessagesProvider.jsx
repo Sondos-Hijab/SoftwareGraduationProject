66f8fe06eec42a3f6bc2bed6a7f1af5f41191e12
Sondos Hijab <sondos.hijab@gmail.com>
2024-06-16 02:12:31 +0300

    Adding needed variables and functions for notifications provider

--- frontend-web/src/Providers/MessagesProvider.jsx
+++ frontend-web/src/Providers/MessagesProvider.jsx
@@ -1,11 +1,19 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { messagesContext as MessagesContext } from "@/store/messages-context";
 import { fetchMessages, getBusinessChatPartners } from "@/apis/chatRequests";
+import { socket } from "@/constants";
+import { createBlobUrl } from "@/utils/utils";
+import placeholderUserPicture from "@/assets/images/placeholder.png";
 
 const MessagesProvider = ({ children }) => {
   const [chatMessages, setChatMessages] = useState([]);
   const [chatPartners, setChatPartners] = useState([]);
-  const [unseenMessagesCount, setUnseenMessagesCount] = useState([]);
+  const [userProfilePicture, setUserProfilePicture] = useState(
+    placeholderUserPicture
+  );
+  const [totalMessagesCount, setTotalMessagesCount] = useState(0);
+
+  const [unseenMessagesCount, setUnseenMessagesCount] = useState(new Map());
 
   const loadChatPartners = () => {
     getBusinessChatPartners().then((value) => {
@@ -16,20 +24,45 @@ const MessagesProvider = ({ children }) => {
   const loadChatMessages = (username) => {
     fetchMessages(username).then((value) => {
       setChatMessages(value.chatMessages);
+      setUserProfilePicture(createBlobUrl(value.userProfilePicture.data));
     });
   };
 
   const addChatToChatMessages = (chatMessage) => {
     setChatMessages((prevMessages) => [...prevMessages, chatMessage]);
+    if (chatMessage.sender == "0")
+      increaseUnseenMessagesCount(chatMessage.userName);
   };
-
   const increaseUnseenMessagesCount = (username) => {
-    setUnseenMessagesCount((prev) => [...prev, { username: prev[username]++ }]);
+    const currentCount = unseenMessagesCount.get(username.toString());
+    if (currentCount === undefined) {
+      setUnseenMessagesCount((prevCount) =>
+        prevCount.set(username.toString(), 1)
+      );
+    } else {
+      setUnseenMessagesCount((prevCount) =>
+        prevCount.set(username.toString(), currentCount + 1)
+      );
+    }
+    setTotalMessagesCount((prev) => prev + 1);
   };
 
   const resetUnseenMessagesCount = (username) => {
-    setUnseenMessagesCount((prev) => [...prev, { username: 0 }]);
+    let previousCount = unseenMessagesCount.get(username);
+    setUnseenMessagesCount((prev) => {
+      return prev.set(username.toString(), 0);
+    });
+    setTotalMessagesCount((prev) => prev - previousCount);
   };
+  useEffect(() => {
+    socket.on("newChatMessage", (newMessage) => {
+      addChatToChatMessages(newMessage);
+    });
+
+    return () => {
+      socket.off("newChatMessage");
+    };
+  }, []);
 
   return (
     <MessagesContext.Provider
@@ -42,6 +75,8 @@ const MessagesProvider = ({ children }) => {
         addChatToChatMessages,
         increaseUnseenMessagesCount,
         resetUnseenMessagesCount,
+        userProfilePicture,
+        totalMessagesCount,
       }}
     >
       {children}
