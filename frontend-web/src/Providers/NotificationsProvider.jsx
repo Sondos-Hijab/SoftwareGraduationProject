0b0afb7644bbfea128a2559fe8ded3c60ba5574c
Sondos Hijab <sondos.hijab@gmail.com>
2024-06-15 13:37:14 +0300

    creating notifications provider and notifications context

---
+++ frontend-web/src/Providers/NotificationsProvider.jsx
@@ -0,0 +1,66 @@
+import { useState, useContext, useEffect } from "react";
+import { notificationsContext as NotificationsContext } from "@/store/notification-context";
+import { fetchFeedback } from "@/apis/feedbackRequests";
+import { socket } from "@/constants";
+const NotificationsProvider = ({ children }) => {
+  const [notifications, setNotifications] = useState([]);
+  const [notificationsCount, setNotificationsCount] = useState(0);
+
+  const getNotifications = () => {
+    fetchFeedback().then((value) => {
+      if (value?.error) {
+        console.log("error fetching notifications");
+      } else {
+        setNotifications(value.feedback);
+      }
+    });
+  };
+
+  const increaseNotificationsCount = () => {
+    setNotificationsCount((prevCount) => prevCount + 1);
+  };
+  const resetNotificationsCount = () => {
+    setNotificationsCount(0);
+  };
+  const decreaseNotificationsCount = () => {
+    setNotificationsCount((prevCount) => prevCount - 1);
+  };
+
+  const addNotification = (newNotification) => {
+    setNotifications((prev) => [...prev, newNotification]);
+  };
+
+  useEffect(() => {
+    socket.emit("register", {
+      businessName: localStorage.getItem("businessName"),
+    });
+
+    socket.on("newFeedback", (newFeedback) => {
+      addNotification(newFeedback);
+    });
+
+    return () => {
+      socket.off("newFeedback");
+    };
+  }, []);
+
+  return (
+    <NotificationsContext.Provider
+      value={{
+        notifications,
+        notificationsCount,
+        getNotifications,
+        addNotification,
+        increaseNotificationsCount,
+        decreaseNotificationsCount,
+        resetNotificationsCount,
+      }}
+    >
+      {children}
+    </NotificationsContext.Provider>
+  );
+};
+
+export default NotificationsProvider;
+
+export const useNotificationsContext = () => useContext(NotificationsContext);
---
+++ frontend-web/src/store/notification-context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const notificationsContext = createContext({
+  notifications: [],
+  notificayionsCount: 0,
+});
