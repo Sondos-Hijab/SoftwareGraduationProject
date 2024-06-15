bd79524f33f6ac3ab4d272a2f099dcc1b1f42242
Sondos Hijab <sondos.hijab@gmail.com>
2024-06-15 16:20:58 +0300

    decrease notifications count when clicking on a notification

--- frontend-web/src/helper-components/Cards/NotificationCard.jsx
+++ frontend-web/src/helper-components/Cards/NotificationCard.jsx
@@ -1,10 +1,13 @@
+import { useNotificationsContext } from "@/Providers/NotificationsProvider";
 import { createBlobUrl, getFormattedDateAndTime } from "@/utils/utils";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const NotificationCard = ({ feedback }) => {
+  const { decreaseNotificationsCount } = useNotificationsContext();
+
   return (
-    <div>
+    <div onClick={decreaseNotificationsCount}>
       <Link
         className="border p-4 flex justify-between bg-slate-50"
         to={`/notifications/${feedback["feedbackID"]}`}
